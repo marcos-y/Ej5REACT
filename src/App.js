d614d270186ab641558faa40b0707fef73c1f7d6
marcos-y <yoldimarcos@gmail.com>
2020-03-03 17:13:17 -0300

    cambios2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Paleta from './Paleta'
 
-//mi app
 function App() {
   return (
     <>
--- src/Paleta.js
+++ src/Paleta.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import './color.css';
 import './paleta.css';
 import './divpadre.css';
+import './listgroup.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
 
 const Paleta = () => {
 
-    const [color,setColor] = useState("");
+    const [color, setColor] = useState("");
     const [colores, setColores] = useState([]);
 
     const handleChangeColor = event => setColor(event.target.value);
-    
-    const handleSubmit = () =>{
-        setColores([...colores,color]);
+
+    const handleSubmit = () => {
+        setColores([...colores, color]);
         console.log(colores);
         setColor("");
     }
@@ -32,6 +35,27 @@ const Paleta = () => {
 
                 </div>
                 <Button variant="primary" onClick={handleSubmit}>Guardar</Button>
+            </div>
+
+            <div className="listgroup">
+                                 
+                <ListGroup>{
+                    colores.map(color => {
+                        return  <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                          <Card.Title>{color}</Card.Title>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                      </Card>
+                    })
+                }
+                </ListGroup>
+              
             </div>
         </>
     )
